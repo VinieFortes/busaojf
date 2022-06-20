c5d136198820df83b588409f8532af0caaea2591
vinieFortes <vinie4apps@gmail.com>
2022-06-20 09:39:55 -0300

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,212 @@
+<template>
+  <q-layout>
+    <q-header class="q-pa-sm flex row items-center bg-white text-black">
+      <q-icon class="q-pa-sm" size="25px" name="menu" style="cursor: pointer" @click="toggleLeftDrawer"></q-icon>
+      <span class="q-pl-md" v-if="!focusSearchBar" style="flex: 1; font-size: 22px">Busão JF</span>
+      <q-icon @click="showSearchBar" style="cursor: pointer" class="q-animate--scale" v-if="showSearch === false" size="sm" name="search"></q-icon>
+      <q-input v-model="pesquisaText" style="flex: 1" @focusout="searchBarOnfocus(false)" @focusin="searchBarOnfocus(true)" autofocus class="q-animate--fade q-pl-md" v-else hide-bottom-space borderless label="Pesquisar Linhas" >
+        <template v-slot:after>
+          <q-icon name="search"></q-icon>
+        </template>
+      </q-input>
+    </q-header>
+
+    <q-drawer elevated  v-model="rightDrawerOpen" side="left" bordered class="flex column">
+      <q-img src="../assets/art.png"></q-img>
+      <div class="flex column">
+        <span class="q-pa-sm">Gostou do App ? Então avalie-o na Play Store!</span>
+        <a style="text-decoration: none" href="https://play.google.com/store/apps/details?id=com.vinie4app.busaojf" class="flex">
+          <q-btn style="flex: 1" color="primary" icon="reviews" label="Avaliar"></q-btn>
+        </a>
+      </div>
+      <q-space/>
+      <div class="flex justify-end q-pa-sm">
+        <div class="row items-center q-gutter-x-sm flex">
+          <span>Feito por Vinie Fortes</span>
+          <a href="https://github.com/VinieFortes" style="font-size: 16px" class="fa fa-github text-black"></a>
+        </div>
+        <span>Ultima Atualização dos horários: 18/06/2022</span>
+      </div>
+    </q-drawer>
+
+    <q-page-container>
+
+
+      <q-card v-if="pesquisaText" class="q-mt-sm flex column">
+        <q-card-section class="no-padding flex justify-center">
+          <div class="flex items-center q-gutter-x-sm">
+            <q-icon name="search"></q-icon>
+            <span>Sua pesquisa</span>
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <q-list v-for="(linha) in searchEngine()" class="q-mb-sm" bordered separator>
+            <q-item @click="this.$router.push({path: '/Horario', query: {linha: showNumber(linha)}})" class="q-gutter-x-sm" clickable v-ripple>
+              <q-badge :color="colorBus(linha)">{{showNumber(linha)}}</q-badge>
+              <q-item-section>{{ showName(linha) }}</q-item-section>
+            </q-item>
+          </q-list>
+        </q-card-section>
+      </q-card>
+
+      <q-card v-if="listaFav.length > 0" class="q-mt-sm flex column">
+        <q-card-section class="no-padding flex justify-center">
+          <div class="flex items-center q-gutter-x-sm">
+            <q-icon name="star"></q-icon>
+            <span>Linhas favoritas</span>
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <q-list v-for="(linha) in listaFav" class="q-mb-sm" bordered separator>
+            <q-item @click="this.$router.push({path: '/Horario', query: {linha: linha.numero}})" class="q-gutter-x-sm" clickable v-ripple>
+              <q-badge :color="colorBus(linha, true)" >{{linha.numero}}</q-badge>
+              <q-item-section>{{ linha.nome }}</q-item-section>
+            </q-item>
+          </q-list>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="q-mt-sm flex column">
+        <q-card-section class="no-padding flex justify-center">
+          <div class="flex items-center q-gutter-x-sm">
+            <q-icon name="directions_bus"></q-icon>
+            <span>Todas as linhas</span>
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <q-list v-for="(linha) in json" class="q-mb-sm" bordered separator>
+            <q-item @click="this.$router.push({path: '/Horario', query: {linha: showNumber(linha)}})" class="q-gutter-x-sm" clickable v-ripple>
+              <q-badge :color="colorBus(linha)">{{showNumber(linha)}}</q-badge>
+              <q-item-section>{{ showName(linha) }}</q-item-section>
+            </q-item>
+          </q-list>
+        </q-card-section>
+      </q-card>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script lang="ts">
+
+import { defineComponent, ref } from 'vue'
+import dados from "assets/output.json"
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+  },
+
+  mounted() {
+    if (cordova.platformId == 'android') {
+      StatusBar.overlaysWebView(false);
+      StatusBar.backgroundColorByHexString("#FFFFFF");
+      StatusBar.styleDefault()
+    }
+  },
+
+  methods:{
+    searchEngine(){
+      let newArray: { numero: number; nome: string; H_ida: { semana: string[]; sabado: string[]; domingo: string[] }[]; H_volta: { semana: string[]; sabado: string[]; domingo: string[] }[]; intinerario: { ida: string; volta: string }[] }[][] = []
+      dados.forEach(linha =>{
+        newArray.push(linha.filter(val => (val.nome.toUpperCase().includes(this.pesquisaText.toUpperCase())) || val.numero.toString().startsWith(this.pesquisaText)))
+      })
+      return newArray.filter(val => val.length > 0);
+    },
+
+    toggleLeftDrawer(){
+      this.rightDrawerOpen = !this.rightDrawerOpen
+    },
+    showNumber(linha: any[]){
+      let numero = null
+      linha.forEach((item)=>{
+        numero = item.numero
+      })
+      return numero;
+    },
+    showName(linha: any[]){
+      let nome = null
+      linha.forEach((item)=>{
+        nome = item.nome
+      })
+      return nome;
+    },
+    showSearchBar(){
+      this.showSearch = !this.showSearch
+    },
+    searchBarOnfocus(args: any){
+      this.focusSearchBar = args
+      if(args === false && !this.pesquisaText){
+        this.showSearch = false;
+      }
+    },
+    colorBus(linha: { numero: number; forEach: (arg0: (item: any) => void) => void }, isFav: any){
+      let color = null
+      if(isFav){
+        if(linha.numero < 300){
+          color = 'primary'
+        }else if(linha.numero < 600 && linha.numero > 300){
+          color = 'negative'
+        }else{
+          color = 'secondary'
+        }
+        return color;
+      }else{
+        linha.forEach((item)=>{
+          if(item.numero < 300){
+            color = 'primary'
+          }else if(item.numero < 600 && item.numero > 300){
+            color = 'negative'
+          }else{
+            color = 'secondary'
+          }
+        })
+        return color;
+      }
+    }
+  },
+
+
+  data() {
+   const favoritos = window.localStorage.getItem('favoritos');
+   let listaFavoritos: { numero: number; nome: string }[] = [];
+   let dadosLocalStorage = [];
+   let showBar = false;
+   let focusSerachBar = false;
+
+    if(window.localStorage.getItem('favoritos')){
+      dadosLocalStorage = JSON.parse(window.localStorage.getItem('favoritos') as string);
+      dadosLocalStorage.forEach((dado: string) =>{
+        dados.forEach(linha=>{
+          linha.forEach(item =>{
+            if(item.numero === parseInt(dado)){
+               listaFavoritos.push({numero: item.numero, nome: item.nome})
+            }
+          })
+        })
+      })
+    }
+
+
+    return {
+      json: dados,
+      rightDrawerOpen: ref(false),
+      favoritos: favoritos,
+      listaFav: listaFavoritos,
+      pesquisaText: ref(''),
+      showSearch: showBar,
+      focusSearchBar: focusSerachBar,
+    }
+  }
+})
+</script>
+
+<style scoped>
+*{
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
---
+++ src/pages/Horario.vue
@@ -0,0 +1,247 @@
+<template>
+  <q-layout>
+    <q-header :style="{backgroundColor: headerColor}"  class="q-pa-sm flex row items-center text-white no-wrap">
+      <q-icon class="q-pa-sm" v-ripple size="25px" name="arrow_back" style="cursor: pointer; flex: 0" @click="this.$router.back()"></q-icon>
+      <span class="q-pl-md" style="font-size: 22px; flex: 1; text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">{{linha}} - {{ nome }}</span>
+      <q-icon @click="showInti" size="25px" class="q-pr-sm" name="directions"></q-icon>
+      <q-icon v-if="!isFavorito" @click="favoritar" v-ripple size="25px" name="star_outline"></q-icon>
+      <q-icon v-else @click="favoritar" v-ripple size="25px" name="star"></q-icon>
+    </q-header>
+
+    <q-page-container class="q-mb-md">
+      <span class="q-mt-md flex justify-center" style="flex: 1; font-size: 16px; font-weight: bold;">Saída de {{nome.split('/')[0]}}</span>
+      <q-card style="border-top-right-radius: 6px; border-top-left-radius: 6px" class="q-mt-sm flex column">
+        <span class="q-mb-sm text-white text-center" :style="{fontSize: '16px', fontWeight: 'bold', backgroundColor: headerColor, flex: '1', borderTopRightRadius: '6px', borderTopLeftRadius: '6px'}">Dias úteis</span>
+        <q-card-section class="q-pa-sm flex column items-center justify-center">
+            <td class="td q-gutter-sm">
+              <q-badge :color="badgeColor" style="font-size: 15px; font-weight: bold" v-for="horario in h_ida_semana">{{horario}}</q-badge>
+            </td>
+        </q-card-section>
+      </q-card>
+
+      <q-card style="border-top-right-radius: 6px; border-top-left-radius: 6px" class="q-mt-sm flex column">
+        <span class="q-mb-sm text-white text-center" :style="{fontSize: '16px', fontWeight: 'bold', backgroundColor: headerColor, flex: '1', borderTopRightRadius: '6px', borderTopLeftRadius: '6px'}">Sábados</span>
+        <q-card-section class="q-pa-sm flex column items-center justify-center">
+          <td class="td q-gutter-sm">
+            <q-badge :color="badgeColor" style="font-size: 15px; font-weight: bold" v-for="horario in h_ida_sabado">{{horario}}</q-badge>
+          </td>
+        </q-card-section>
+      </q-card>
+
+      <q-card style="border-top-right-radius: 6px; border-top-left-radius: 6px" class="q-mt-sm flex column">
+        <span class="q-mb-sm text-white text-center" :style="{fontSize: '16px', fontWeight: 'bold', backgroundColor: headerColor, flex: '1', borderTopRightRadius: '6px', borderTopLeftRadius: '6px'}">Domingos e Feriados</span>
+        <q-card-section class="q-pa-sm flex column items-center justify-center">
+          <td class="td q-gutter-sm">
+            <q-badge :color="badgeColor" style="font-size: 15px; font-weight: bold" v-for="horario in h_ida_domingo">{{horario}}</q-badge>
+          </td>
+        </q-card-section>
+      </q-card>
+
+      <span class="q-mt-md flex justify-center" style="flex: 1; font-size: 16px; font-weight: bold;">Saída de {{!nome.split('/')[1] ? 'Centro' : nome.split('/')[1]}}</span>
+
+      <q-card style="border-top-right-radius: 6px; border-top-left-radius: 6px" class="q-mt-sm flex column">
+        <span class="q-mb-sm text-white text-center" :style="{fontSize: '16px', fontWeight: 'bold', backgroundColor: headerColor, flex: '1', borderTopRightRadius: '6px', borderTopLeftRadius: '6px'}">Dias úteis</span>
+        <q-card-section class="q-pa-sm flex column items-center justify-center">
+          <td class="td q-gutter-sm">
+            <q-badge :color="badgeColor" style="font-size: 15px; font-weight: bold" v-for="horario in h_volta_semana">{{horario}}</q-badge>
+          </td>
+        </q-card-section>
+      </q-card>
+
+      <q-card style="border-top-right-radius: 6px; border-top-left-radius: 6px" class="q-mt-sm flex column">
+        <span class="q-mb-sm text-white text-center" :style="{fontSize: '16px', fontWeight: 'bold', backgroundColor: headerColor, flex: '1', borderTopRightRadius: '6px', borderTopLeftRadius: '6px'}">Sábados</span>
+        <q-card-section class="q-pa-sm flex column items-center justify-center">
+          <td class="td q-gutter-sm">
+            <q-badge :color="badgeColor" style="font-size: 16px; font-weight: bold" v-for="horario in h_volta_sabado">{{horario}}</q-badge>
+          </td>
+        </q-card-section>
+      </q-card>
+
+      <q-card style="border-top-right-radius: 6px; border-top-left-radius: 6px" class="q-mt-sm flex column">
+        <span class="q-mb-sm text-white text-center" :style="{fontSize: '16px', fontWeight: 'bold', backgroundColor: headerColor, flex: '1', borderTopRightRadius: '6px', borderTopLeftRadius: '6px'}">Domingos e Feriados</span>
+        <q-card-section class="q-pa-sm flex column items-center justify-center">
+          <td class="td q-gutter-sm">
+            <q-badge :color="badgeColor" style="font-size: 15px; font-weight: bold" v-for="horario in h_volta_domingo">{{horario}}</q-badge>
+          </td>
+        </q-card-section>
+      </q-card>
+
+    </q-page-container>
+
+    <q-dialog v-model="showDialog">
+      <q-card class="q-dialog-plugin">
+        <div class="flex items-center justify-center q-gutter-x-sm row q-pa-sm">
+          <q-icon style="flex: 0" size="23px" name="directions"></q-icon>
+          <span style="font-weight: bold; font-size: 16px; flex: 1" >Itinerário {{linha}} - {{nome}}</span>
+        </div>
+        <q-card-section class="flex">
+          <q-card class="flex">
+            <span class="q-mb-sm text-white text-center" :style="{fontSize: '16px', fontWeight: 'bold', backgroundColor: headerColor, flex: '1', borderTopRightRadius: '6px', borderTopLeftRadius: '6px'}">IDA</span>
+            <span class="q-pa-sm">{{inti_ida.split(',').join(', ')}}</span>
+          </q-card>
+        </q-card-section>
+        <q-card-section class="flex">
+          <q-card class="flex">
+            <span class="q-mb-sm text-white text-center" :style="{fontSize: '16px', fontWeight: 'bold', backgroundColor: headerColor, flex: '1', borderTopRightRadius: '6px', borderTopLeftRadius: '6px'}">VOLTA</span>
+            <span class="q-pa-sm">{{inti_volta.split(',').join(', ')}}</span>
+          </q-card>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+
+  </q-layout>
+</template>
+
+<script>
+import dados from "assets/output.json"
+import {useQuasar} from "quasar";
+export default {
+  name: "Horario",
+
+  methods: {
+    showInti(){
+      this.showDialog = true
+    },
+    favoritar(){
+      this.isFavorito = !this.isFavorito;
+      this.showNotif(this.isFavorito, this.linha);
+      let dadosLocalStorage = [];
+      if(window.localStorage.getItem('favoritos')){
+        dadosLocalStorage = JSON.parse(window.localStorage.getItem('favoritos'));
+        if(this.isFavorito){
+          dadosLocalStorage.push(this.linha)
+        }else{
+          dadosLocalStorage = dadosLocalStorage.filter(item => item !== this.linha)
+        }
+        window.localStorage.setItem('favoritos', JSON.stringify(dadosLocalStorage));
+      }else{
+        window.localStorage.setItem('favoritos', JSON.stringify([this.linha]))
+      }
+
+    }
+  },
+
+  mounted() {
+    let dadosLocalStorage = [];
+    if(window.localStorage.getItem('favoritos')){
+      dadosLocalStorage = JSON.parse(window.localStorage.getItem('favoritos'));
+      dadosLocalStorage.includes(this.linha) === true ? this.isFavorito = true : this.isFavorito = false;
+    }
+  },
+
+  data(){
+    let linha = ''
+    linha = this.$route.query.linha;
+    let nome = ''
+    let h_ida_semana = null
+    let h_ida_sabado = null
+    let h_ida_domingo = null
+
+    let h_volta_semana = null
+    let h_volta_sabado = null
+    let h_volta_domingo = null
+
+    let int_ida = null
+    let int_volta = null
+
+    let headerColor = '#FFFFFF';
+    let badgeColor = 'primary';
+
+    let isFavorito = false;
+
+    const $q = useQuasar()
+
+    if(linha < 300){
+      headerColor = '#1976D2';
+      badgeColor = 'primary'
+      if (cordova.platformId === 'android') {
+        StatusBar.overlaysWebView(false);
+        StatusBar.styleBlackTranslucent()
+        StatusBar.backgroundColorByHexString("#1976D2");
+      }
+    }
+    else if(linha > 300 && linha < 600){
+      headerColor = '#C10015';
+      badgeColor = 'negative'
+      if (cordova.platformId === 'android') {
+        StatusBar.overlaysWebView(false);
+        StatusBar.styleBlackTranslucent()
+        StatusBar.backgroundColorByHexString("#C10015");
+      }
+    }else{
+      headerColor = '#26A69A';
+      badgeColor = 'secondary'
+      if (cordova.platformId === 'android') {
+        StatusBar.overlaysWebView(false);
+        StatusBar.styleBlackTranslucent()
+        StatusBar.backgroundColorByHexString("#26A69A");
+      }
+    }
+
+    dados.forEach(item=>{
+      item.forEach((el)=>{
+       if(parseInt(linha) === el.numero){
+         nome = el.nome
+         el.H_ida.forEach(ida =>{
+           h_ida_semana = ida.semana
+           h_ida_sabado = ida.sabado
+           h_ida_domingo = ida.domingo
+         })
+         el.H_volta.forEach(volta =>{
+           h_volta_semana = volta.semana
+           h_volta_sabado = volta.sabado
+           h_volta_domingo = volta.domingo
+         })
+         el.intinerario.forEach(int =>{
+           int_ida = int.ida
+           int_volta = int.volta
+         })
+       }
+      })
+    })
+
+    return{
+      linha: linha,
+      json: dados,
+      nome: nome,
+      h_ida_semana: h_ida_semana,
+      h_ida_sabado: h_ida_sabado,
+      h_ida_domingo: h_ida_domingo,
+      h_volta_semana: h_volta_semana,
+      h_volta_sabado: h_volta_sabado,
+      h_volta_domingo: h_volta_domingo,
+      inti_ida: int_ida,
+      inti_volta: int_volta,
+      headerColor: headerColor,
+      badgeColor: badgeColor,
+      isFavorito: isFavorito,
+      showNotif (fav, linha) {
+        if(fav){
+          $q.notify({
+            message: `${this.nome} foi  adicionado aos favoritos`,
+            icon: 'star_rate',
+            color: 'primary',
+            timeout: 1000
+          })
+        }else{
+          $q.notify({
+            message: `${this.nome} foi removido dos favoritos`,
+            icon: 'delete_forever',
+            color: 'negative',
+            timeout: 1000
+          })
+        }
+      },
+      showDialog: false
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.td{
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+}
+</style>
